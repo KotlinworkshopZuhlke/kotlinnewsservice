18b5783da0b23000a7b297014224ac22f5f79b6a
cehe <celine.heldner@zuehlke.com>
2024-10-29 14:52:31 +0100

    Initial Spring Boot App generated from Spring Bioot initializer

---
+++ src/test/kotlin/com/zuehlke/kotlin/news/NewsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.zuehlke.kotlin.news
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NewsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
