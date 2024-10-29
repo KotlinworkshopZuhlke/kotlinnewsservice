206d1f8844435f78950ca62290acd5a86d029dd1
cehe <celine.heldner@zuehlke.com>
2024-10-29 17:23:13 +0100

    Add default article to testnews

--- src/main/kotlin/com/zuehlke/kotlin/news/controller/NewsController.kt
+++ src/main/kotlin/com/zuehlke/kotlin/news/controller/NewsController.kt
@@ -1,5 +1,7 @@
 package com.zuehlke.kotlin.news.controller
 
+import com.zuehlke.kotlin.news.model.Article
+import com.zuehlke.kotlin.news.model.ArticleSource
 import com.zuehlke.kotlin.news.model.NewsFeed
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController
 class NewsController {
 
     @GetMapping("api/testnews")
-    fun fetchTestNews() = NewsFeed(status = "", totalResults = 0, articles = listOf())
+    fun fetchTestNews() = NewsFeed(status = "", totalResults = 0, articles = listOf(
+        Article(source = ArticleSource())
+    ))
 
 }
--- src/test/kotlin/com/zuehlke/kotlin/news/controller/NewsControllerTest.kt
+++ src/test/kotlin/com/zuehlke/kotlin/news/controller/NewsControllerTest.kt
@@ -13,16 +13,30 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 class NewsControllerTest(@Autowired val mockMvc: MockMvc) {
 
     @Test
-    fun `when api_testnews is called then response is an empty Newsfeed`() {
+    fun `when api_testnews is called then response is a Newsfeed with a default acticle`() {
         mockMvc.perform(get("/api/testnews")).andExpect(status().isOk)
             .andExpect(content().contentType(MediaType.APPLICATION_JSON)).andExpect(
                 content().json(
                     """
-                      {
-                        "status":"",
-                        "totalResults":0,
-                        "articles":[]
-                      }
+                     {
+                      "status": "",
+                      "totalResults": 0,
+                      "articles": [
+                        {
+                          "source": {
+                            "id": null,
+                            "name": null
+                          },
+                          "author": null,
+                          "title": null,
+                          "description": null,
+                          "url": null,
+                          "urlToImage": null,
+                          "publishedAt": null,
+                          "content": null
+                        }
+                      ]
+                    } 
                       """
                         .trimIndent()
                 )
