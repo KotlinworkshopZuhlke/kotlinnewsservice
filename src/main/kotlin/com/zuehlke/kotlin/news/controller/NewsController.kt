3cc7112a6ccd2f6ff0fbd948fe34b924f4fc2a79
cehe <celine.heldner@zuehlke.com>
2024-10-30 15:12:20 +0100

    Rename Dataservice

--- src/main/kotlin/com/zuehlke/kotlin/news/controller/NewsController.kt
+++ src/main/kotlin/com/zuehlke/kotlin/news/controller/NewsController.kt
@@ -1,6 +1,6 @@
 package com.zuehlke.kotlin.news.controller
 
-import com.zuehlke.kotlin.news.domain.NewsRepository
+import com.zuehlke.kotlin.news.domain.DataService
 import com.zuehlke.kotlin.news.model.Article
 import com.zuehlke.kotlin.news.model.ArticleSource
 import com.zuehlke.kotlin.news.model.NewsFeed
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class NewsController(val newsRepository: NewsRepository) {
+class NewsController(val dataService: DataService) {
 
     @GetMapping("api/testnews")
     fun fetchTestNews() = NewsFeed(
@@ -21,6 +21,6 @@ class NewsController(val newsRepository: NewsRepository) {
     TODO Step 5: handle the fetcheNews() Result success or failure with a "getOrThrow" statement.
      */
     @GetMapping("api/news")
-    fun fetchNews() = newsRepository.fetchNews()
+    fun fetchNews() = dataService.fetchNews()
 
 }
--- src/main/kotlin/com/zuehlke/kotlin/news/domain/DataService.kt
+++ src/main/kotlin/com/zuehlke/kotlin/news/domain/DataService.kt
@@ -2,7 +2,7 @@ package com.zuehlke.kotlin.news.domain
 
 import com.zuehlke.kotlin.news.model.NewsFeed
 
-interface NewsRepository {
+interface DataService {
 
     fun fetchNews(): NewsFeed //TODO Step 4: Adapt the return type
 }
