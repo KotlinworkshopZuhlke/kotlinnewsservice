f220cb70740a00df50a5dcaab3497472b5ab7ec4
cehe <celine.heldner@zuehlke.com>
2024-10-30 10:23:00 +0100

    Add API Key

--- src/main/kotlin/com/zuehlke/kotlin/news/NewsApplication.kt
+++ src/main/kotlin/com/zuehlke/kotlin/news/NewsApplication.kt
@@ -5,15 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.web.reactive.function.client.WebClient
+import java.net.URI
 
 @SpringBootApplication
 class NewsApplication {
     @Value("\${newsapi.baseUrl}")
     lateinit var newsApiBaseUrl: String
 
+    @Value("\${newsapi.apikey}")
+    lateinit var newsApiKey: String
+
     @Bean
     fun newsWebClient(): WebClient {
-        return WebClient.builder().baseUrl(newsApiBaseUrl).build()
+        return WebClient.builder().baseUrl("${newsApiBaseUrl}?sources=bbc-news&apiKey=${newsApiKey}").build()
     }
 
 }
