51874bc5438b578b4c9041d27716df77325a86c9
cehe <celine.heldner@zuehlke.com>
2024-10-31 09:45:24 +0100

    Fix test

--- src/test/kotlin/com/zuehlke/kotlin/news/controller/NewsControllerTest.kt
+++ src/test/kotlin/com/zuehlke/kotlin/news/controller/NewsControllerTest.kt
@@ -1,6 +1,6 @@
 package com.zuehlke.kotlin.news.controller
 
-import com.zuehlke.kotlin.news.data.service.NewsServiceRemote
+import com.zuehlke.kotlin.news.domain.DataService
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
@@ -18,14 +18,14 @@ class NewsControllerTest {
     lateinit var mockMvc: MockMvc
 
     @MockBean
-    lateinit var newsServiceRemote: NewsServiceRemote
+    lateinit var dataService: DataService
 
     @Test
     fun `when api_testnews is called then response is a Newsfeed with a default acticle`() {
         mockMvc.perform(get("/api/testnews")).andExpect(status().isOk)
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
             .andExpect(
-                content().json (
+                content().json(
                     """
                      {
                       "status": "",
