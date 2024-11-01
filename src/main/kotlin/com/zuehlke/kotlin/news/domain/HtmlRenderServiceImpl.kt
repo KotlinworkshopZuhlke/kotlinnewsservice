d952be2e5f9ba7e0ca5208a5c973e7f70341a045
cehe <celine.heldner@zuehlke.com>
2024-11-01 18:14:23 +0100

    Add description for the first HTML

--- src/main/kotlin/com/zuehlke/kotlin/news/domain/HtmlRenderServiceImpl.kt
+++ src/main/kotlin/com/zuehlke/kotlin/news/domain/HtmlRenderServiceImpl.kt
@@ -1,6 +1,5 @@
 package com.zuehlke.kotlin.news.domain
 
-import com.zuehlke.kotlin.news.domain.htmldsl.html
 import com.zuehlke.kotlin.news.model.Article
 import org.springframework.stereotype.Service
 
@@ -8,23 +7,24 @@ import org.springframework.stereotype.Service
 class HtmlRenderServiceImpl : HtmlRenderService {
     override fun renderArticle(article: Article): String {
 
-        return html {
-            head {
-                title { +"News Article from News Service!" }
-            }
-            body {
-                h1 { +"${article.title}" }
-
-                // an element with attributes and text content
-                a(href = "${article.url}") { +"Link to Article" }
-
-                // mixed content
-                p {
-                    +"${article.description}"
-                    b { +" -${article.author}" }
-                }
-                img(src = "${article.urlToImage}") {}
-            }
-        }.toString()
+//        return html {
+//            head {
+//                title { +"News Article from News Service!" }
+//            }
+//            body {
+//                h1 { +"${article.title}" }
+//
+//                // an element with attributes and text content
+//                a(href = "${article.url}") { +"Link to Article" }
+//
+//                // mixed content
+//                p {
+//                    +"${article.description}"
+//                    b { +" -${article.author}" }
+//                }
+//                img(src = "${article.urlToImage}") {}
+//            }
+//        }.toString()
+        TODO()
     }
 }
