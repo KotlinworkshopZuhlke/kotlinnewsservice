683024197eefad343ff798f2b52b808e67f6481f
cehe <celine.heldner@zuehlke.com>
2024-11-10 12:48:30 +0100

    Add description for the exercise

---
+++ src/main/kotlin/com/zuehlke/kotlin/news/controller/model/NewsFeed.kt
@@ -0,0 +1,9 @@
+package com.zuehlke.kotlin.news.controller.model
+
+/*
+TODO step 1:
+ We will fetch a news feed from an HTTP REST API
+ add here the relevant classes for the NewsFeed.
+ The NewsFeed will look like the one inside [/src/main/resources/localdata/newsfile.json]
+ implement the classes and properties according to the json.
+ */
